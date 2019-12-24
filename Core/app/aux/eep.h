76bee8f49040f0c23fe81037b041707ed89934d7
mas <base2embedded@gmail.com>
2019-12-24 19:48:28 +0330

    add eep lib to the project 	modified:   Core/app/aux/eep.c 	modified:   Core/app/aux/eep.h

--- Core/app/aux/eep.h
+++ Core/app/aux/eep.h
@@ -10,4 +10,29 @@
 
 
 
+
+#include "main.h"
+
+
+#define _EEPROM_SIZE_KBIT					32
+#define	_EEPROM24XX_I2C						hi2c1
+#define	_EEPROM_FREERTOS_IS_ENABLE			0
+#define	_EEPROM_USE_WP_PIN					0
+
+#if (_EEPROM_USE_WP_PIN==1)
+#define	_EEPROM_WP_GPIO						EE_WP_GPIO_Port
+#define	_EEPROM_WP_PIN						EE_WP_Pin
+#endif
+
+
+
+uint8_t EEPROM24XX_IsConnected(void);
+uint8_t EEPROM24XX_Save(uint16_t Address,void *data,size_t size_of_data);
+uint8_t EEPROM24XX_Load(uint16_t Address,void *data,size_t size_of_data);
+
+
+
+
+
+
 #endif /* APP_AUX_EEP_H_ */
