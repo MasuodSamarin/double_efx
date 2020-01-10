04b2122536819af9a2f21f40e9292db7ae1e44da
mas <base2embedded@gmail.com>
2020-01-10 17:23:18 +0330

    add comment on the FSM 	modified:   Core/app/fsm_funcs.c

--- Core/app/fsm_funcs.c
+++ Core/app/fsm_funcs.c
@@ -9,6 +9,9 @@
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * HELPER FUNCTION USED IN FSM
+ * */
 char tmp_str[20];
 
 #define VOL_X		8
@@ -63,10 +66,33 @@ void Helper_Print_EFX_ADC_Vol(Vol_t vol, Vol_Name_t name){
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*
+ * ACTUAL FSM FUNCTION
+ * ALL OF THE GAME
+ * */
 
+
+/*
+ * Reset state:
+ * 	1. INIT handle ADT
+ * 	2, load vital values from EEP
+ * 	3, make link-list from EEP data chain
+ * 	4, set handle->cur_efx to the last enabled effect
+ * */
 void fp_S0_Reset(App_Handle_t *handle){}
 
-
+/*
+ * State 1: All Events
+ * 	1. important parameter is handle->cur_efx
+ * 	2. set HC595 shift register values
+ * 	3. set PWM values
+ * 	4. refresh LCD
+ * 		A. effect name
+ * 		B, effect number
+ * 		C, effect volumes value like horizontal bar with small pin
+ * 	5. button push time = Long push time
+ * 	6. goto straight STATE 2
+ * */
 void fp_S1_All(App_Handle_t *handle){
 	handle->cur_efx = Effect_List_Get_Element(Enc_Event_Get_val);
 	glcd_clear_buffer();
@@ -87,14 +113,104 @@ void fp_S1_All(App_Handle_t *handle){
 
 }
 
+
+
+
+/*
+ * State 2:	Not Events
+ * 	1. Is it FACTORY-MODE effects?
+ * 		yes:
+ * 			1. all volumes refresh it's value. reading their value's from ADC
+ * 			2. update PWM's
+ * 			3. update LCD. volume's view
+ * 		no:
+ * 			1. if any volume changes its default value, do PWM and LCD update accordingly
+ * 	2. note that a desire volume must been in effect group volumes
+ * */
 void fp_S2_Not(App_Handle_t *handle){}
+
+
+
+
+/*
+ * State 2: Button Press Event
+ * 	1. Is it FACTORY-MODE effects?
+ * 		yes:
+ * 			1. save the new effect on tail of link-list
+ * 		no:
+ * 			1. update volume's value to the already exist effect
+ * 	2. goto the effect
+ * 	3. update the EEP value
+ * 	4. goto the STATE 1
+ * */
 void fp_S2_Btn(App_Handle_t *handle){}
+
+
+
+
+/*
+ * State 2: Encoder Changes Event
+ * 	1. set handle->tmp_efx to next or previous effect on the link-list
+ * 	2. button push time = Normal push time
+ * 	3. goto the State 3
+ * */
 void fp_S2_Enc(App_Handle_t *handle){}
+
+
+
+
+/*
+ * State 2: Volume Changes Event
+ * 	1. is volume belong to the Volume-Group?
+ * 		yes:
+ * 			1. mark that volume to online or free vol
+ * 		no:
+ * 			1. there's nothing to do
+ * */
 void fp_S2_Vol(App_Handle_t *handle){}
 
+
+
+/*
+ * State 3:	Not Events
+ * 	1. important parameter is handle->tmp_efx
+ * 	2. show tmp effect on the LCD.
+ * 		A)name	B)Number	C)volume group
+ * 	3. the B section blinks every 100ms
+ * 	4. after some time-out:
+ * 		A) change Encoder tim value to handle->cur_efx
+ * 		B) goto State 2
+ * */
 void fp_S3_Not(App_Handle_t *handle){}
+
+
+
+
+/*
+ * State 3: Button Press Event
+ * 	1. set handle->cur_efx =  handle->tmp_efx
+ * 	2. goto State 1
+ * */
 void fp_S3_Btn(App_Handle_t *handle){}
+
+
+
+
+/*
+ * State 3: Encoder Changes Event
+ * 	1. set handle->tmp_efx to next or previous effect on the link-list
+ * 	2. set blink timer
+ * */
 void fp_S3_Enc(App_Handle_t *handle){}
+
+
+
+
+/*
+ * State 3: Volume Changes Event
+ * 	1. change Encoder tim value to handle->cur_efx
+ * 	2. goto State 2
+ * */
 void fp_S3_Vol(App_Handle_t *handle){}
 
 
